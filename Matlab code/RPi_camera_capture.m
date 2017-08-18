35be21d5554ebab4f0b43cb27870b1de91f2e24f
Pablo Johnson <paulj.96@gmail.com>
2017-08-18 14:55:54 -0500

    Bugfix

--- Matlab code/RPi_camera_capture.m
+++ Matlab code/RPi_camera_capture.m
@@ -3,7 +3,7 @@ function RPi_camera_capture(rpi_object, pi_output_path, num_frames, wavelength, 
 % exposure. Uses pi_output_path, wavelength, num_frames, and exposure to
 % create appropriate directory for output data.
 
-fprint('Taking photos');
+fprintf('Taking photos');
 pi_output_path = [pi_output_path '/' num2str(wavelength) '/'];
 ssh2_command(rpi_object, ['python3 /home/pi/Desktop/I2R-MonoChrometer/PiCode/PiCamera_Capture.py ' pi_output_path ' ' num2str(num_frames) ' ' num2str(exposure)]);
 
